fb1cbd87994521c50eb1c5fde3790764985b360c
Mira Estil <imz.estil@gmail.com>
2016-10-17 15:20:51 -0400

    Cats

--- AC3.2-InstaCats-1/View Controllers/InstaCatTableViewController.swift
+++ AC3.2-InstaCats-1/View Controllers/InstaCatTableViewController.swift
@@ -9,35 +9,133 @@
 import UIKit
 
 struct InstaCat {
-  // add necessary ivars and initilizations; check the tests to know what you should have
+    // add necessary ivars and initilizations; check the tests to know what you should have
+    let name: String
+    let catID: Int
+    let instagramURL: URL
+    var description: String {
+        return "Nice to meet you, I'm \(name)"
+    }
 }
 
 class InstaCatTableViewController: UITableViewController {
-
+    
+    internal let InstaCatTableViewCellIdentifier: String = "InstaCatCellIdentifier"
+    internal let instaCatJSONFileName: String = "InstaCats.json"
+    internal var instaCats: [InstaCat] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        guard let instaCatsURL: URL = self.getResourceURL(from: instaCatJSONFileName),
+            let instaCatData: NSData = self.getData(from: instaCatsURL) as NSData?,
+            let instaCatsAll: [InstaCat] = self.getInstaCats(from: instaCatData as Data) else {
+                return
+        }
+        
+        self.instaCats = instaCatsAll
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return instaCats.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "InstaCatCellIdentifier", for: indexPath)
+        
+        cell.textLabel?.text = instaCats[indexPath.row].name
+        cell.detailTextLabel?.text = instaCats[indexPath.row].description
+        
         return cell
     }
-
+    
+    //MARK: - NSURL Functions
+    internal func getResourceURL(from fileName: String) -> URL? {
+        
+        // 1. There are many ways of doing this parsing, we're going to practice String traversal
+        guard let dotRange = fileName.rangeOfCharacter(from: CharacterSet.init(charactersIn: ".")) else {
+            return nil
+        }
+        
+        // 2. The upperbound of a range represents the position following the last position in the range, thus we can use it
+        // to effectively "skip" the "." for the extension range
+        let fileNameComponent: String = fileName.substring(to: dotRange.lowerBound)
+        let fileExtenstionComponent: String = fileName.substring(from: dotRange.upperBound)
+        
+        // 3. Here is where Bundle.main comes into play
+        let fileURL: URL? = Bundle.main.url(forResource: fileNameComponent, withExtension: fileExtenstionComponent)
+        
+        return fileURL
+    }
+    
+    internal func getData(from url: URL) -> Data? {
+        
+        // 1. this is a simple handling of a function that can throw. In this case, the code makes for a very short function
+        // but it can be much larger if we change how we want to handle errors.
+        let fileData: Data? = try? Data(contentsOf: url)
+        return fileData
+    }
+    
+    internal func getInstaCats(from jsonData: Data) -> [InstaCat]? {
+        var appendCats = [InstaCat]() //need local var
+        
+        // 1. This time around we'll add a do-catch
+        do {
+            let instaCatJSONData: Any = try JSONSerialization.jsonObject(with: jsonData, options: [])
+            
+            // 2. Cast from Any into a more suitable data structure and check for the "cats" key
+            // 3. Check for keys "name", "cat_id", "instagram", making sure to cast values as needed along the way
+            if let catDict = instaCatJSONData as? [String:[[String:Any]]] {
+                if let catData = catDict["cats"] {
+                    for cats in catData {
+                        if let name = cats["name"] as? String,
+                            let idString = cats["cat_id"] as? String,
+                            let instagramURL = cats["instagram"] as? String {
+                            
+                            guard let id = Int(idString), let instagramURL = URL(string: instagramURL) else {return nil}
+                            
+                            let instagramCat = InstaCat(name: name, catID: id, instagramURL: instagramURL)
+                            appendCats.append(instagramCat)
+ 
+                            
+                            /* //If you use this, would have to convert a few parameters when putting value in TVC
+                             let instagramCat = InstaCat(name: name, catID: idString, instagramURL: instagramURL)
+                             
+                             instaCats.append(appendCats)
+                            */
+                            
+                        }
+                    }
+                }
+            }
+        }
+        catch let error as NSError {
+            // JSONSerialization doc specficially says an NSError is returned if JSONSerialization.jsonObject(with:options:) fails
+            print("Error occurred while parsing data: \(error.localizedDescription)")
+        }
+        return  appendCats
+    }
+    
+    //MARK: Delegate
+    
+    //loads Instagram URL
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        UIApplication.shared.open(instaCats[indexPath.row].instagramURL)
+    }
 }
+
+
+
+
+
+
